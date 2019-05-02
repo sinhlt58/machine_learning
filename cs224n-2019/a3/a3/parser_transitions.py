99c976ead47fe5df364c3e8dafeaecb587682073
sinhlt58 <sinhlt58@gmail.com>
2019-05-02 17:46:39 +0700

    finised cs224n a3 part_c

--- cs224n-2019/a3/a3/parser_transitions.py
+++ cs224n-2019/a3/a3/parser_transitions.py
@@ -7,6 +7,7 @@ Sahil Chopra <schopra8@stanford.edu>
 """
 
 import sys
+import copy
 
 class PartialParse(object):
     def __init__(self, sentence):
@@ -30,7 +31,9 @@ class PartialParse(object):
         ###
         ### Note: The root token should be represented with the string "ROOT"
         ###
-
+        self.stack = ['ROOT']
+        self.buffer = copy.deepcopy(sentence)
+        self.dependencies = []
 
         ### END YOUR CODE
 
@@ -49,7 +52,14 @@ class PartialParse(object):
         ###         1. Shift
         ###         2. Left Arc
         ###         3. Right Arc
-
+        if transition == 'S' and len(self.buffer) > 0:
+            self.stack.append(self.buffer.pop(0))
+        elif transition == 'LA' and len(self.stack) > 1:
+            self.dependencies.append((self.stack[-1], self.stack[-2]))
+            self.stack.pop(-2)
+        elif transition == 'RA' and len(self.stack) > 1:
+            self.dependencies.append((self.stack[-2], self.stack[-1]))
+            self.stack.pop(-1)
 
         ### END YOUR CODE
 
@@ -100,6 +110,9 @@ def minibatch_parse(sentences, model, batch_size):
     ###             contains references to the same objects. Thus, you should NOT use the `del` operator
     ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
     ###             is being accessed by `partial_parses` and may cause your code to crash.
+    
+    partial_parses = []
+    
 
 
     ### END YOUR CODE
